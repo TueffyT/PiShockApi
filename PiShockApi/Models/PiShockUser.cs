aa12919173d6483465e06ccffcc4e8554b1b2558
Tueffy <github@tueffy.com>
2023-03-20 12:09:34 +0100

    ApiClient

---
+++ PiShockApi/Models/PiShockUser.cs
@@ -0,0 +1,22 @@
+﻿namespace PiShockApi.Models {
+    public class PiShockUser {
+        public string Username {
+            get;
+            set;
+        }
+
+        public string ApiKey {
+            get;
+            set;
+        }
+
+        public string Code {
+            get;
+            set;
+        }
+        
+        public override string ToString() {
+            return Username;
+        }
+    }
+}
---
+++ PiShockApi/PiShockApiClient.cs
@@ -0,0 +1,101 @@
+using Microsoft.Extensions.Logging;
+using PiShockApi.Enums;
+using PiShockApi.Models;
+using Refit;
+
+namespace PiShockApi {
+    public class PiShockApiClient {
+        private readonly IPiShockApi _piShockApi;
+        private readonly ILogger _logger;
+        
+        public PiShockApiClient( IPiShockApi piShockApi, ILogger<PiShockApiClient> logger = null ) {
+            _piShockApi = piShockApi;
+            _logger = logger;
+        }
+
+        public string DisplayName {
+            get;
+            set;
+        } = "PiShockControl";
+
+        public async Task<PiShockResult> SendShockAsync( PiShockUser piShockUser, int intensity, int duration ) {
+            PiShockRequest request = new() {
+                Username = piShockUser.Username,
+                ApiKey = piShockUser.ApiKey,
+                Code = piShockUser.Code,
+                Name = DisplayName,
+                Operation = (int) PiShockOperation.Shock,
+                Intensity = intensity,
+                Duration = duration
+            };
+            _logger?.LogDebug( "Sending Shock {@Request}", request );
+            ApiResponse<string> response = await _piShockApi.SendPiShockCommandAsync( request ).ConfigureAwait( false );
+
+            if( response.IsSuccessStatusCode && response.Content == "Operation Succeeded." ) {
+                return new PiShockResult( true, response.Content );
+            }
+
+            return new PiShockResult( false, response.Content );
+        }
+
+        public async Task<PiShockResult> SendMiniShockAsync( PiShockUser piShockUser, int intensity ) {
+            PiShockRequest request = new PiShockRequest {
+                Username = piShockUser.Username,
+                ApiKey = piShockUser.ApiKey,
+                Code = piShockUser.Code,
+                Name = DisplayName,
+                Operation = (int) PiShockOperation.Shock,
+                Intensity = intensity,
+                Duration = 300
+            };
+            _logger?.LogDebug( "Sending Mini-Shock {@Request}", request );
+            ApiResponse<string> response = await _piShockApi.SendPiShockCommandAsync( request ).ConfigureAwait( false );
+
+            if( response.IsSuccessStatusCode && response.Content == "Operation Succeeded." ) {
+                return new PiShockResult( true, response.Content );
+            }
+
+            return new PiShockResult( false, response.Content );
+        }
+
+        public async Task<PiShockResult> SendVibrationAsync( PiShockUser piShockUser, int intensity, int duration ) {
+            PiShockRequest request = new PiShockRequest {
+                Username = piShockUser.Username,
+                ApiKey = piShockUser.ApiKey,
+                Code = piShockUser.Code,
+                Name = DisplayName,
+                Operation = (int) PiShockOperation.Vibrate,
+                Intensity = intensity,
+                Duration = duration
+            };
+            _logger?.LogDebug( "Sending Vibration {@Request}", request );
+            ApiResponse<string> response = await _piShockApi.SendPiShockCommandAsync( request ).ConfigureAwait( false );
+
+            if( response.IsSuccessStatusCode && response.Content == "Operation Succeeded." ) {
+                return new PiShockResult( true, response.Content );
+            }
+
+            return new PiShockResult( false, response.Content );
+        }
+
+        public async Task<PiShockResult> SendBeepAsync( PiShockUser piShockUser, int intensity, int duration ) {
+            PiShockRequest request = new PiShockRequest {
+                Username = piShockUser.Username,
+                ApiKey = piShockUser.ApiKey,
+                Code = piShockUser.Code,
+                Name = DisplayName,
+                Operation = (int) PiShockOperation.Beep,
+                Intensity = intensity,
+                Duration = duration
+            };
+            _logger?.LogDebug( "Sending Beep {@Request}", request );
+            ApiResponse<string> response = await _piShockApi.SendPiShockCommandAsync( request );
+
+            if( response.IsSuccessStatusCode && response.Content == "Operation Succeeded." ) {
+                return new PiShockResult( true, response.Content );
+            }
+
+            return new PiShockResult( false, response.Content );
+        }
+    }
+}

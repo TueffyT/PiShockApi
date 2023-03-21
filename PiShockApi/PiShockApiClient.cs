42a42bd69fe2ec806fbb12e927331082bc55acfa
Tueffy <github@tueffy.com>
2023-03-21 07:35:46 +0100

    Logging HotFix

--- PiShockApi/PiShockApiClient.cs
+++ PiShockApi/PiShockApiClient.cs
@@ -14,10 +14,6 @@ namespace PiShockApi {
             _piShockApi = RestService.For<IPiShockApi>( "https://do.pishock.com/api/apioperate" );
         }
 
-        public PiShockApiClient( ILoggerFactory loggerFactory ) : this() {
-            PiShockLogger.Initialize( loggerFactory );
-        }
-
         public string DisplayName {
             get;
             set;
@@ -101,29 +97,6 @@ namespace PiShockApi {
             }
 
             return new PiShockResult( false, response.Content );
-        }
-
-        public static class PiShockLogger {
-            private static ILoggerFactory _loggerFactory;
-
-            private static readonly ConcurrentDictionary<Type, ILogger> _loggerByType = new();
-
-            public static void Initialize( ILoggerFactory loggerFactory ) {
-                if( _loggerFactory is not null ) {
-                    throw new InvalidOperationException( "StaticLogger already initialized!" );
-                }
-
-                _loggerFactory = loggerFactory ?? throw new ArgumentNullException( nameof( loggerFactory ) );
-            }
-
-            public static ILogger GetStaticLogger<T>() {
-                if( _loggerFactory is null ) {
-                    throw new InvalidOperationException( "StaticLogger is not initialized yet." );
-                }
-
-                return _loggerByType
-                    .GetOrAdd( typeof( T ), _loggerFactory.CreateLogger<T>() );
-            }
         }
     }
 }
---
+++ PiShockApi/PiShockLogger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
+
+namespace PiShockApi {
+    public static class PiShockLogger {
+        private static ILoggerFactory _loggerFactory;
+
+        private static readonly ConcurrentDictionary<Type, ILogger> _loggerByType = new();
+
+        public static void Initialize( ILoggerFactory loggerFactory ) {
+            if( _loggerFactory is not null ) {
+                throw new InvalidOperationException( "StaticLogger already initialized!" );
+            }
+
+            _loggerFactory = loggerFactory ?? throw new ArgumentNullException( nameof( loggerFactory ) );
+        }
+
+        public static ILogger GetStaticLogger<T>() {
+            if( _loggerFactory is null ) {
+                throw new InvalidOperationException( "StaticLogger is not initialized yet." );
+            }
+
+            return _loggerByType
+                .GetOrAdd( typeof( T ), _loggerFactory.CreateLogger<T>() );
+        }
+    }
+}

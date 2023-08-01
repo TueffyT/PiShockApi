ef0940ad5e120806d9ce6b520e7ebe26bb198640
Tueffy <121118648+TueffyT@users.noreply.github.com>
2023-08-01 12:29:31 +0200

    Unit Tests (#1)
    * Validate User Tests
    
    * Intensity Tests
    
    * Minimal Cleanup

---
+++ PiShockApi.Tests/ValidateIntensityTest.cs
@@ -0,0 +1,24 @@
+﻿namespace PiShockApi.Tests {
+    public class ValidateIntensityTest {
+        [Theory]
+        [InlineData( 1 )]
+        [InlineData( 20 )]
+        [InlineData( 100 )]
+        [InlineData( 0 )]
+        public void ValidateValidIntensity( int intensity ) {
+            PiShockApiClient apiClient = new();
+            Exception? ex = Record.Exception( () => apiClient.ValidateIntensityAndThrow( intensity ) );
+            Assert.Null( ex );
+        }
+
+
+        [Theory]
+        [InlineData( -1 )]
+        [InlineData( -10 )]
+        [InlineData( 101 )]
+        public void ValidateInvalidIntensity( int intensity ) {
+            PiShockApiClient apiClient = new();
+            Assert.Throws<ArgumentOutOfRangeException>( () => apiClient.ValidateIntensityAndThrow( intensity ) );
+        }
+    }
+}
---
+++ PiShockApi.Tests/ValidateUserTest.cs
@@ -0,0 +1,46 @@
+namespace PiShockApi.Tests;
+
+public class ValidateUserTest {
+    public static IEnumerable<object[]> GetTestUsers() {
+        yield return new object[] {
+            new PiShockUser()
+        };
+        yield return new object[] {
+            new PiShockUser() {
+                ApiKey = "123"
+            }
+        };
+        yield return new object[] {
+            new PiShockUser() {
+                ApiKey = "123", Code = "Code123"
+            }
+        };
+        yield return new object[] {
+            new PiShockUser() {
+                ApiKey = "123", Username = "TestUsername"
+            }
+        };
+    }
+
+    [Fact]
+    public void TestThrowInvalidUserWhenNull() {
+        PiShockApiClient apiClient = new();
+        Assert.Throws<ArgumentNullException>( () => apiClient.ValidateUserAndThrow( null ) );
+    }
+
+    [Theory]
+    [MemberData( nameof( GetTestUsers ) )]
+    public void TestThrowInvalidUserWhenInvalid( PiShockUser user ) {
+        PiShockApiClient apiClient = new();
+        Assert.Throws<ArgumentException>( () => apiClient.ValidateUserAndThrow( user ) );
+    }
+
+    [Fact]
+    public void TestValidUser() {
+        PiShockApiClient apiClient = new();
+        Exception? ex = Record.Exception( () => apiClient.ValidateUserAndThrow( new PiShockUser() {
+            ApiKey = "123", Code = "Code123", Username = "TestUsername"
+        } ) );
+        Assert.Null( ex );
+    }
+}
